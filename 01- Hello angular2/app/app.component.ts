14ed12336ba8a037a834f2a11cddf97fc26b62b8
Shahzad Nawaz <shahzadscs@gmail.com>
2016-04-15 20:39:50 +0500

    Add app root component.

---
+++ 01- Hello angular2/app/app.component.ts
@@ -0,0 +1,12 @@
+/**
+ * Created by Shahzad on 4/15/16.
+ */
+
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>Hello Angular2!</h1>'
+})
+
+export class AppComponent { }
