1b890a99f77a35fe2f34b413e76184b818412431
Shahzad Nawaz <shahzadscs@gmail.com>
2016-04-17 02:13:53 +0500

    Rename app component into heroes component.

---
+++ 02- Tour of heroes/app/heroes.component.ts
@@ -0,0 +1,100 @@
+/**
+ * Created by Shahzad on 4/15/16.
+ */
+
+import {Component, OnInit} from 'angular2/core';
+import {Hero} from './hero';
+import {HeroDetailComponent} from './hero-detail.component';
+import {HeroService} from './hero.service';
+
+
+@Component({
+  selector: 'my-heroes',
+  template: `
+  <h2>My Heroes</h2>
+  <p *ngIf="!heroes">Fetching list...</p>
+  <ul class="heroes">
+    <li *ngFor="#hero of heroes"
+    [class.selected]=" hero === selectedHero"
+    (click)="onSelect(hero)">
+      <!-- each hero goes here -->
+       <span class="badge">{{hero.id}}</span> {{hero.name}}
+    </li>
+  </ul>
+  <hero-detail [hero]="selectedHero"></hero-detail>
+  `,
+  styles: [`
+  .selected {
+    background-color: #CFD8DC !important;
+    color: white;
+  }
+  .heroes {
+    margin: 0 0 2em 0;
+    list-style-type: none;
+    padding: 0;
+    width: 15em;
+  }
+  .heroes li {
+    cursor: pointer;
+    position: relative;
+    left: 0;
+    background-color: #EEE;
+    margin: .5em;
+    padding: .3em 0;
+    height: 1.6em;
+    border-radius: 4px;
+  }
+  .heroes li.selected:hover {
+    background-color: #BBD8DC !important;
+    color: white;
+  }
+  .heroes li:hover {
+    color: #607D8B;
+    background-color: #DDD;
+    left: .1em;
+  }
+  .heroes .text {
+    position: relative;
+    top: -3px;
+  }
+  .heroes .badge {
+    display: inline-block;
+    font-size: small;
+    color: white;
+    padding: 0.8em 0.7em 0 0.7em;
+    background-color: #607D8B;
+    line-height: 1em;
+    position: relative;
+    left: -1px;
+    top: -4px;
+    height: 1.8em;
+    margin-right: .8em;
+    border-radius: 4px 0 0 4px;
+  }
+`],
+  directives: [HeroDetailComponent],
+  providers: []
+})
+
+export class HeroesComponent implements OnInit {
+  heroes: Hero[];
+  selectedHero: Hero;
+
+  constructor(private _heroService: HeroService) {
+
+  }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+  getHeroes() {
+    //this._heroService.getHeroes().then(heroes => this.heroes = heroes);
+    this._heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
+  }
+
+  onSelect(hero: Hero) {
+    this.selectedHero = hero;
+  }
+
+}
