d04b1a88ccd7cd53ce7031421b223eb083df675f
Shahzad Nawaz <shahzadscs@gmail.com>
2016-04-17 02:42:14 +0500

    Define a route for Heroes Component.

--- 02- Tour of heroes/app/app.component.ts
+++ 02- Tour of heroes/app/app.component.ts
@@ -3,6 +3,8 @@
  */
 
 import {Component} from 'angular2/core';
+import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router';
+
 import {HeroService} from './hero.service';
 import {HeroesComponent} from './heroes.component';
 
@@ -10,11 +12,20 @@ import {HeroesComponent} from './heroes.component';
   selector: 'my-app',
   template: `
   <h1>{{title}}</h1>
-  <my-heroes></my-heroes>
+  <a [routerLink]="['Heroes']">Heroes</a>
+  <router-outlet></router-outlet>
   `,
-  directives: [HeroesComponent],
-  providers: [HeroService]
+  directives: [ROUTER_DIRECTIVES],
+  providers: [ROUTER_PROVIDERS, HeroService]
 })
+
+@RouteConfig([
+  {
+    path: '/heroes',
+    name: 'Heroes',
+    component: HeroesComponent
+  }
+])
 
 export class AppComponent {
   title = 'Tour of Heroes';
