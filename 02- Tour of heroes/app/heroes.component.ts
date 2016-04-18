7bbdc5177b1aad1d6bb61cdd8cdfae5554873ece
Shahzad Nawaz <shahzadscs@gmail.com>
2016-04-18 19:51:11 +0500

    Add view and styles files for heroes component.

--- 02- Tour of heroes/app/heroes.component.ts
+++ 02- Tour of heroes/app/heroes.component.ts
@@ -3,6 +3,8 @@
  */
 
 import {Component, OnInit} from 'angular2/core';
+import {Router} from 'angular2/router';
+
 import {Hero} from './hero';
 import {HeroDetailComponent} from './hero-detail.component';
 import {HeroService} from './hero.service';
@@ -10,77 +12,16 @@ import {HeroService} from './hero.service';
 
 @Component({
   selector: 'my-heroes',
-  template: `
-  <h2>My Heroes</h2>
-  <p *ngIf="!heroes">Fetching list...</p>
-  <ul class="heroes">
-    <li *ngFor="#hero of heroes"
-    [class.selected]=" hero === selectedHero"
-    (click)="onSelect(hero)">
-      <!-- each hero goes here -->
-       <span class="badge">{{hero.id}}</span> {{hero.name}}
-    </li>
-  </ul>
-  <hero-detail [hero]="selectedHero"></hero-detail>
-  `,
-  styles: [`
-  .selected {
-    background-color: #CFD8DC !important;
-    color: white;
-  }
-  .heroes {
-    margin: 0 0 2em 0;
-    list-style-type: none;
-    padding: 0;
-    width: 15em;
-  }
-  .heroes li {
-    cursor: pointer;
-    position: relative;
-    left: 0;
-    background-color: #EEE;
-    margin: .5em;
-    padding: .3em 0;
-    height: 1.6em;
-    border-radius: 4px;
-  }
-  .heroes li.selected:hover {
-    background-color: #BBD8DC !important;
-    color: white;
-  }
-  .heroes li:hover {
-    color: #607D8B;
-    background-color: #DDD;
-    left: .1em;
-  }
-  .heroes .text {
-    position: relative;
-    top: -3px;
-  }
-  .heroes .badge {
-    display: inline-block;
-    font-size: small;
-    color: white;
-    padding: 0.8em 0.7em 0 0.7em;
-    background-color: #607D8B;
-    line-height: 1em;
-    position: relative;
-    left: -1px;
-    top: -4px;
-    height: 1.8em;
-    margin-right: .8em;
-    border-radius: 4px 0 0 4px;
-  }
-`],
-  directives: [HeroDetailComponent],
-  providers: []
+  templateUrl: 'app/heroes.component.html',
+  styleUrls: ['app/heroes.component.css'],
+  directives: [HeroDetailComponent]
 })
 
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
   selectedHero: Hero;
 
-  constructor(private _heroService: HeroService) {
+  constructor(private _heroService: HeroService, private _router: Router) {
 
   }
 
@@ -95,6 +36,10 @@ export class HeroesComponent implements OnInit {
 
   onSelect(hero: Hero) {
     this.selectedHero = hero;
+  }
+
+  gotoDetail() {
+    this._router.navigate(['HeroDetail', {id: this.selectedHero.id}]);
   }
 
 }
