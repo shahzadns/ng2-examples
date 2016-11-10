af5ce6ce6bb951aef8c56e7930e1bc276ed0c73b
Shahzad Nawaz <shahzadscs@gmail.com>
2016-11-11 04:06:58 +0500

    Fix TS Error - Duplicate Identifier for Ambient lib.

--- 01- Hello angular2/app/app.component.ts
+++ 01- Hello angular2/app/app.component.ts
@@ -6,7 +6,7 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: '<h1>Hello Angular2.0!</h1>'
+  template: '<h1>Hello Angular2!</h1>'
 })
 
 export class AppComponent { }
