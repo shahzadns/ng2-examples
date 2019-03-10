e7dfc66a1a8b56a215d2f1c1a7524a1cc9f0e9b0
Shahzad Nawaz <shahzadscs@gmail.com>
2019-03-10 19:31:28 +0500

    02b - implement heroes list.

--- 02- Tour of heroes - cli/src/app/heroes/heroes.component.ts
+++ 02- Tour of heroes - cli/src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -9,6 +10,7 @@ import { Hero } from '../hero';
 })
 export class HeroesComponent implements OnInit {
 
+  heroes = HEROES;
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
