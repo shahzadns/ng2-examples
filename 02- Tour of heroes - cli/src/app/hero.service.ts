62cf0c386995253a43dc5908e215bbdc72fd3d0f
Shahzad Nawaz <shahzadscs@gmail.com>
2019-03-10 21:14:41 +0500

    02b - implement Rxjs observable and custom/mock onetime emit based observable.

--- 02- Tour of heroes - cli/src/app/hero.service.ts
+++ 02- Tour of heroes - cli/src/app/hero.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
 
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
@@ -12,8 +13,10 @@ export class HeroService {
     console.log('HeroService: constructor:');
   }
 
-  getHeroes(): Hero[] {
-    return HEROES;
+  getHeroes(): Observable<Hero[]> {
+
+    // returns a resolved (onetime emit based) observable
+    return of(HEROES);
   }
 
 }
--- 02- Tour of heroes - cli/src/app/heroes/heroes.component.ts
+++ 02- Tour of heroes - cli/src/app/heroes/heroes.component.ts
@@ -31,7 +31,8 @@ export class HeroesComponent implements OnInit {
   }
 
   getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes)
   }
 
 }
